154500cc607f8b40c70187c827f1b81c3d57c0cb
ninjaJason86 <jsntrout@gmail.com>
2024-04-07 14:44:08 +0200

    Updating the code for the authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,20 @@ app.use(express.json());
 
 app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req, res, next) {
-    //Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(request, response, next) {
+    if (!request.session["authorization"]) {
+        return response.status(403).json({ message: "User not logged in" })
+    }
+
+    const token = request.session["authorization"]['accessToken'];
+    jwt.verify(token, "access", (error, user) => {
+        if (error) {
+            return response.status(403).json({ message: "User not authenticated" })
+        }
+
+        request["user"] = user;
+        next();
+    });
 });
 
 const PORT = 5000;
